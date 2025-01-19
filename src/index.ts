5a9fcdbcf50d38ab78471547926739aa16928494
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2025-01-18 22:53:38 -0500

    using cloudflare workers ai with binding vs ai sdk

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,57 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.json`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// index.ts
+import { createWorkersAI } from 'workers-ai-provider';
+import { streamText } from 'ai';
+
+type Env = {
+	AI: Ai;
+};
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(req: Request, env: Env) {
+		const path = new URL(req.url).pathname;
+
+		if (path == '/ai-sdk') {
+			const workersai = createWorkersAI({ binding: env.AI });
+			// Use the AI provider to interact with the Vercel AI SDK
+			// Here, we generate a chat stream based on a prompt
+			const text = await streamText({
+				model: workersai('@cf/meta/llama-2-7b-chat-int8'),
+				messages: [
+					{
+						role: 'user',
+						content: 'Write an essay about hello world',
+					},
+				],
+			});
+
+			return text.toTextStreamResponse({
+				headers: {
+					// add these headers to ensure that the
+					// response is chunked and streamed
+					'Content-Type': 'text/x-unknown',
+					'content-encoding': 'identity',
+					'transfer-encoding': 'chunked',
+				},
+			});
+		} else if (path === '/vanilla-ai-binding') {
+			const responseStream = await env.AI.run('@cf/meta/llama-3.1-8b-instruct', {
+				stream: true,
+				messages: [{ role: 'user', content: 'Write an essay about hello world' }],
+				max_tokens: 512,
+			});
+
+			return new Response(responseStream as ReadableStream, {
+				headers: {
+					'content-type': 'text/event-stream',
+					'cache-control': 'no-cache',
+					connection: 'keep-alive',
+				},
+			});
+		}
+
+		return new Response('<a href="/ai-sdk">AI SDK</a><br /><a href="/vanilla-ai-binding">Vanilla AI Binding</a>', {
+			status: 200,
+			headers: { 'content-type': 'text/html' },
+		});
 	},
-} satisfies ExportedHandler<Env>;
+};
